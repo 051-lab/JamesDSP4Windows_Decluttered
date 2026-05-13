856ea59a95f937aefc5c9221e3b9907e0d959857
kimutimuti <kimutimuti@gmail.com>
2026-05-13 18:49:26 +0900

    Delete JamesDSP-Windows/build_temp.bat

--- JamesDSP-Windows/build_temp.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-if %errorlevel% neq 0 exit /b %errorlevel%
-cmake -S . -B build-fix -G Ninja
-if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build build-fix --config Release
-if %errorlevel% neq 0 exit /b %errorlevel%
