8f8358f188e9302ae01e1938e7eded0bc7c824d1
kimutimuti <kimutimuti@gmail.com>
2026-05-13 19:14:40 +0900

    Delete JamesDSP-Windows/build directory

--- JamesDSP-Windows/build/.qt/QtDeployTargets-RelWithDebInfo.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_JamesDSP-GUI_FILE C:/Users/gmaym/.gemini/antigravity/playground/cosmic-meteor/JamesDSP-Windows/build/RelWithDebInfo/JamesDSP-GUI.exe)
-set(__QT_DEPLOY_TARGET_JamesDSP-GUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_JamesDSP-GUI_RUNTIME_DLLS D:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgets.dll;D:/Qt/6.10.1/msvc2022_64/bin/Qt6Gui.dll;D:/Qt/6.10.1/msvc2022_64/bin/Qt6Core.dll)
